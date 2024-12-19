58c966401ecd77d50a12ad0e2b53181d4be7696a
AJ APARICIO <aparicio.anthonyjohn@gmail.com>
2024-12-19 22:44:16 +0800

    Enable host network

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,4 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // This exposes the server to your network
+    port: 3000, // Optional: specify port
+  },
 });
