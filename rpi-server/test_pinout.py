9e033c05f0f9b95f17763e4e0624a69a71f1e2bc
AJ APARICIO <aparicio.anthonyjohn@gmail.com>
2024-12-19 23:31:06 +0800

    Change pin heading

---
+++ rpi-server/test_pinout.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+import time
+
+# Just test a single pin first
+TEST_PIN = 27
+
+GPIO.setwarnings(False)  # Disable warnings temporarily for testing
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(TEST_PIN, GPIO.IN)
+
+print(f"Testing pin {TEST_PIN}")
+print(f"Current value: {GPIO.input(TEST_PIN)}")
+
+GPIO.cleanup()
